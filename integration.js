2a64c4d42e197720f2feca30f414909deb12d015
Ed <ed@polarity.io>
2018-01-27 00:59:12 -0500

    Refactor code into various methods

--- integration.js
+++ integration.js
@@ -4,14 +4,14 @@ let request = require('request');
 let _ = require('lodash');
 let util = require('util');
 let net = require('net');
+let config = require('./config/config');
 let async = require('async');
-var log = null;
+let fs = require('fs');
+let log = null;
 let requestWithDefaults;
 
-const BASE_URI = 'https://api.dnsdb.info/lookup/rrset/name/';
+const DOMAIN_URI = 'https://api.dnsdb.info/lookup/rrset/name/';
 const IP_URI = 'https://api.dnsdb.info/lookup/rdata/ip/';
-//const THROTTLE_INTERVAL = 3000;
-//const THROTTLE_MAX_REQUESTS = 1;
 
 function startup(logger) {
     log = logger;
@@ -48,32 +48,32 @@ function doLookup(entities, options, cb) {
     let entitiesWithNoData = [];
     let lookupResults = [];
 
-    if(typeof(options.apiKey) !== 'string' || options.apiKey.length === 0){
+    if (typeof(options.apiKey) !== 'string' || options.apiKey.length === 0) {
         cb("The API key is not set.");
         return;
     }
 
     async.each(entities, function (entityObj, next) {
         if (_.includes(blacklist, entityObj.value)) {
-                rnext(null);
+            rnext(null);
         }
-        else if (entityObj.isDomain && options.lookupDomain)
-         {
-            _lookupEntity(entityObj, options, function (err, result) {
+        else if (entityObj.isDomain && options.lookupDomain) {
+            _lookupEntityDomain(entityObj, options, function (err, result) {
                 if (err) {
                     next(err);
                 } else {
-                    lookupResults.push(result); log.debug({result: result}, "Checking the result values ");
+                    lookupResults.push(result);
+                    log.debug({result: result}, "Checking the result values ");
                     next(null);
                 }
             });
-        } else if (options.lookupIp && entityObj.isIPv4 || entityObj.isIPv6)
-        {
+        } else if (options.lookupIp && entityObj.isIPv4 || entityObj.isIPv6) {
             _lookupEntityIP(entityObj, options, function (err, result) {
                 if (err) {
                     next(err);
                 } else {
-                    lookupResults.push(result); log.debug({result: result}, "Checking the result values ");
+                    lookupResults.push(result);
+                    log.debug({result: result}, "Checking the result values ");
                     next(null);
                 }
             });
@@ -86,199 +86,243 @@ function doLookup(entities, options, cb) {
     });
 }
 
-
-function _lookupEntity(entityObj, options, cb) {
-
-    if(entityObj.value)
-        request({
+function _lookupEntityDomain(entityObj, options, cb) {
+    let requestOptions = {
         uri: DOMAIN_URI + entityObj.value.toLowerCase(),
         method: 'GET',
         qs: {
             limit: 10,
             time_last_after: -31536000
         },
-            headers: {
-                'X-API-Key': options.apiKey,
-                'Accept': 'application/json'
-            },
-            json: true
-        }, function (err, response, body) {
-            if (err) {
-                cb(err);
-                return;
-            }
+        headers: {
+            'X-API-Key': options.apiKey,
+            'Accept': 'application/json'
+        },
+        json: true
+    };
 
     requestWithDefaults(requestOptions, function (err, response, body) {
         let errorObject = _isApiError(err, response, body, entityObj.value);
         if (errorObject) {
-                }));
-                return;
-            }
+            cb(errorObject);
+            return;
+        }
 
-            if (response.statusCode !== 200) {
-                cb(body);
-                return;
-            }
-            log.debug({body: body}, "Printing out the results of Body ");
-
-
-            if( body.length < 2){
-                return;
-            }
-            let splitBody = body.split("\n");
-
-            let bodyObjects = _.map(splitBody, function(row){
-                if(row.length > 3)
-                    return JSON.parse(row);
-                else
-                    return {};
-            });
-
-            log.debug({bodyObj: bodyObjects}, "parsed data");
-
-
-
-            bodyObjects.forEach(function(obj){
-                obj.time_first = obj.time_first*1000;
-                obj.time_last = obj.time_last*1000;
-            });
-
-            // The lookup results returned is an array of lookup objects with the following format
+        if (_isLookupMiss(response)) {
             cb(null, {
-                // Required: This is the entity object passed into the integration doLookup method
                 entity: entityObj,
-                // Required: An object containing everything you want passed to the template
-                data: {
-                    // Required: this is the string value that is displayed in the template
-                    entity_name: entityObj.value,
-                    // Required: These are the tags that are displayed in your template
-                    summary: [bodyObjects[1].rrname],
-                    // Data that you want to pass back to the notification window details block
-                    details: {
-                        bodyObjects: bodyObjects
-
-                    }
-                }
+                data: null
             });
+            return;
+        }
+
+        log.debug({body: body}, "Printing out the results of Body ");
+
+        let bodyObjects = _processRequestBody(body);
+
+        // this will only happen if all JSON rows couldn't be parsed so we'll treat it as an error
+        if(bodyObjects.length === 0){
+            cb(_createJsonErrorPayload("Unable to parse result body into JSON", null, '500', '6', 'JSON Parse Failure', {
+                responseBody: body
+            }));
+            return;
+        }
+
+        log.debug({bodyObj: bodyObjects}, "parsed data");
+
+        _mutateBodyObjects(bodyObjects);
+
+        // The lookup results returned is an array of lookup objects with the following format
+        cb(null, {
+            // Required: This is the entity object passed into the integration doLookup method
+            entity: entityObj,
+            // Required: An object containing everything you want passed to the template
+            data: {
+                // Required: These are the tags that are displayed in your template
+                summary: [bodyObjects[1].rrname],
+                // Data that you want to pass back to the notification window details block
+                details: {
+                    bodyObjects: bodyObjects
+                }
+            }
         });
+    });
 }
 
 function _lookupEntityIP(entityObj, options, cb) {
+    let requestOptions = {
+        uri: IP_URI + entityObj.value,
+        method: 'GET',
+        qs: {
+            limit: 10
+        },
+        headers: {
+            'X-API-Key': options.apiKey,
+            'Accept': 'application/json'
+        },
+        json: true
+    };
 
-    if(entityObj.value)
-        request({
-            uri: IP_URI + entityObj.value,
-            method: 'GET',
-            headers: {
-                'X-API-Key': options.apiKey,
-                'Accept': 'application/json'
-            },
-            json: true
     requestWithDefaults(requestOptions, function (err, response, body) {
-            if (err) {
-                cb(err);
-                return;
-            }
+        let errorObject = _isApiError(err, response, body, entityObj.value);
+        if (errorObject) {
+            cb(errorObject);
+            return;
+        }
 
-            if(response.statusCode === 429){
-                cb(_createJsonErrorPayload("Daily DNSDB API Lookup Limit Reached", null, '429', '2A', 'Lookup Limit Reached', {
-                    err: err
-                }));
-                return;
-            }
-
-            if(response.statusCode === 503){
-                cb(_createJsonErrorPayload("Limit of number of concurrent connections is exceeded for DNSDB", null, '503', '2A', 'Concurrent Connections Exceeded', {
-                    err: err
-                }));
-                return;
-            }
-
-            if(response.statusCode === 400){
-                cb(_createJsonErrorPayload("URL formatted incorrectly", null, '400', '2A', 'Bad Request', {
-                    err: err
-                }));
-                return;
-            }
-
-            if(response.statusCode === 500){
-                cb(_createJsonErrorPayload("Error processing your request", null, '500', '2A', 'Internal Server Error', {
-                    err: err,
-                    entity: entityObj.value
-                }));
-                return;
-            }
-
-
-            if(response.statusCode === 403){
-                cb(_createJsonErrorPayload("You do not have permission to access DNSDB. Please check your API key", null, '403', '2A', 'Permission Denied', {
-                    err: err
-                }));
-                return;
-            }
-
-            if(response.statusCode === 404){
-                cb(_createJsonErrorPayload("Entity not found in DNSDB", null, '404', '2A', 'Entity not found', {
-                    err: err,
-                    entity: entityObj.value
-                }));
-                return;
-            }
-
-            if (response.statusCode !== 200) {
-                cb(body);
-                return;
-            }
-            log.debug({body: body}, "Printing out the results of Body ");
-
-
-
-            let splitBody = body.split("\n");
-
-            let bodyObjects = _.map(splitBody, function(row){
-                if(row.length > 3)
-                    return JSON.parse(row);
-                else
-                    return {};
-            });
-
-            log.debug({bodyObj: bodyObjects}, "parsed data");
-
-            log.debug({bodyObjTest: bodyObjects[1]}, "Body objects 1 test");
-
-
-            log.debug({bodyOBject: bodyObjects}, "bodyobjects new");
-
-            bodyObjects.forEach(function(obj){
-                obj.time_first = obj.time_first*1000;
-                obj.time_last = obj.time_last*1000;
-                obj.rdata = [obj.rdata];
-            });
-
-            // The lookup results returned is an array of lookup objects with the following format
+        if (_isLookupMiss(response)) {
             cb(null, {
-                // Required: This is the entity object passed into the integration doLookup method
                 entity: entityObj,
-                // Required: An object containing everything you want passed to the template
-                data: {
-                    // Required: this is the string value that is displayed in the template
-                    entity_name: entityObj.value,
-                    // Required: These are the tags that are displayed in your template
-                    summary: [bodyObjects[1].rrname],
-                    // Data that you want to pass back to the notification window details block
-                    details: {
-                        bodyObjects: bodyObjects
-
-                    }
-                }
+                data: null
             });
+            return;
+        }
+
+        log.debug({body: body}, "Request Body for IP Lookup");
+
+        let bodyObjects = _processRequestBody(body);
+
+        // this will only happen if all JSON rows couldn't be parsed so we'll treat it as an error
+        if(bodyObjects.length === 0){
+            cb(_createJsonErrorPayload("Unable to parse result body into JSON", null, '500', '6', 'JSON Parse Failure', {
+                responseBody: body
+            }));
+            return;
+        }
+
+        log.debug({bodyObj: bodyObjects}, "parsed data");
+        log.debug({bodyObjTest: bodyObjects[1]}, "Body objects 1 test");
+        log.debug({bodyObject: bodyObjects}, "bodyobjects new");
+
+        // Mutates the body objects to modify some properties and make them easier to render
+        _mutateBodyObjects(bodyObjects);
+
+        // The lookup results returned is an array of lookup objects with the following format
+        cb(null, {
+            // Required: This is the entity object passed into the integration doLookup method
+            entity: entityObj,
+            // Required: An object containing everything you want passed to the template
+            data: {
+                // Required: These are the tags that are displayed in your template
+                summary: [bodyObjects[1].rrname],
+                // Data that you want to pass back to the notification window details block
+                details: {
+                    bodyObjects: bodyObjects
+                }
+            }
         });
+    });
+}
+
+/**
+ * The response body from DNSDB is text consisting of JSON objects in string format that are newline delimited.
+ * As a result, we have to manually parse this into a javascript object
+ * @param body
+ * @returns {*} A javascript array containing DNSDB data objects
+ * @private
+ */
+function _processRequestBody(body){
+    if(typeof body !== 'string' || body.length === 0){
+        return [];
+    }
+
+    let splitBody = body.split("\n");
+
+    let bodyObjects = splitBody.reduce((result, rowAsString) => {
+        try{
+            let parsedRow = JSON.parse(rowAsString);
+            result.push(parsedRow);
+        }catch(e){
+            // invalid JSON so skip it
+        }
+        return result;
+    }, []);
+
+    return bodyObjects;
+}
+
+/**
+ * Mutates body objects by modifying them to be suitable for passing to our template
+ * @param object
+ * @private
+ */
+function _mutateBodyObjects(bodyObjects){
+    bodyObjects.forEach(function (obj) {
+        if(obj.time_first){
+            obj.time_first = obj.time_first * 1000;
+        }
+
+        if(obj.time_last){
+            obj.time_last = obj.time_last * 1000;
+        }
+
+        if(obj.zone_time_first){
+            obj.zone_time_first = obj.zone_time_first * 1000;
+        }
+
+        if(obj.zone_time_last){
+            obj.zone_time_last = obj.zone_time_last * 1000;
+        }
+
+        if(obj.rdata && !Array.isArray(obj.rdata)) {
+            obj.rdata = [obj.rdata];
+        }
+    });
+}
+
+function _isLookupMiss(response) {
+    return response.statusCode === 404;
+}
+
+function _isApiError(err, response, body, entityValue) {
+    if (err) {
+        return err;
+    }
+
+    if (response.statusCode === 429) {
+        return _createJsonErrorPayload("Daily DNSDB API Lookup Limit Reached", null, '429', '1', 'Lookup Limit Reached', {
+            err: err
+        });
+    }
+
+    if (response.statusCode === 503) {
+        return _createJsonErrorPayload("Limit of number of concurrent connections is exceeded for DNSDB", null, '503', '2', 'Concurrent Connections Exceeded', {
+            err: err
+        });
+    }
+
+    if (response.statusCode === 400) {
+        return _createJsonErrorPayload("URL formatted incorrectly", null, '400', '3', 'Bad Request', {
+            err: err
+        });
+    }
+
+    if (response.statusCode === 500) {
+        return _createJsonErrorPayload("Error processing your request", null, '500', '4', 'Internal Server Error', {
+            err: err,
+            entity: entityValue
+        });
+    }
+
+
+    if (response.statusCode === 403) {
+        return _createJsonErrorPayload("You do not have permission to access DNSDB. Please check your API key", null, '403', '5', 'Permission Denied', {
+            err: err
+        });
+    }
+
+    // Any code that is not 200 and not 404 (missed response), we treat as an error
+    if (response.statusCode !== 200 && response.statusCode !== 404) {
+        return body;
+    }
+
+    return null;
 }
 
 function validateOptions(userOptions, cb) {
     let errors = [];
-    if(typeof userOptions.apiKey.value !== 'string' ||
-        (typeof userOptions.apiKey.value === 'string' && userOptions.apiKey.value.length === 0)){
+    if (typeof userOptions.apiKey.value !== 'string' ||
+        (typeof userOptions.apiKey.value === 'string' && userOptions.apiKey.value.length === 0)) {
         errors.push({
             key: 'apiKey',
             message: 'You must provide a Farsight DNSDB API key'
