821ed68edb814e52a4646c7c183acfd30ea7ff8c
Ed <ed@polarity.io>
2018-01-27 00:57:50 -0500

    General config cleanup

--- config/config.js
+++ config/config.js
@@ -1,18 +1,20 @@
 module.exports = {
-    "name": "FarsightDNSDB",
-    "acronym":"PDNS",
-    "logging": { level: 'debug'},
-    "description": "Farsight DNSDB Passive DNS api integration",
-    "entityTypes": ['domain', 'IPv4', 'IPv6'],
-    "styles":[
+    name: "Farsight DNSDB",
+    acronym: "PDNS",
+    logging: {
+        level: "debug"
+    },
+    description: "Farsight DNSDB Passive DNS api integration",
+    entityTypes: ['domain', 'IPv4', 'IPv6'],
+    styles: [
         "./styles/dnsdb.less"
     ],
-    "block": {
-        "component": {
-            "file": "./components/dnsdb.js"
+    block: {
+        component: {
+            file: "./components/dnsdb.js"
         },
-        "template": {
-            "file": "./templates/dnsdb.hbs"
+        template: {
+            file: "./templates/dnsdb.hbs"
         }
     },
     request: {
@@ -32,34 +34,42 @@ module.exports = {
         // the url parameter (by embedding the auth info in the uri)
         proxy: ""
     },
+    options: [
         {
-            "key"         : "blacklist",
-            "name"        : "Blacklist Domains",
-            "description" : "List of domains that you never want to send to DNSDB",
-            "default"     : "default value",
-            "type"        : "text",
-            "userCanEdit" : false,
-            "adminOnly"    : false
-        },
-
-        {
-            "key": "lookupDomain",
-            "name": "Lookup Domains",
-            "description": "If checked, the integration will lookup Domains",
-            "default": true,
-            "type": "boolean",
-            "userCanEdit": true,
-            "adminOnly": false
+            key: "apiKey",
+            name: "API Key",
+            description: "DNSDB API key",
+            type: "text",
+            default: "",
+            userCanEdit: false,
+            adminOnly: false
         },
         {
-            "key": "lookupIp",
-            "name": "Lookup IPv4 and IPv6 Addresses",
-            "description": "If checked, the integration will lookup IPv4 and IPv6 addresses",
-            "default": true,
-            "type": "boolean",
-            "userCanEdit": true,
-            "adminOnly": false
+            key: "blacklist",
+            name: "Blacklist Domains",
+            description: "List of domains that you never want to send to DNSDB",
+            default: "",
+            type: "text",
+            userCanEdit: false,
+            adminOnly: false
+        },
+        {
+            key: "lookupDomain",
+            name: "Lookup Domains",
+            description: "If checked, the integration will lookup Domains",
+            default: true,
+            type: "boolean",
+            userCanEdit: true,
+            adminOnly: false
+        },
+        {
+            key: "lookupIp",
+            name: "Lookup IPv4 and IPv6 Addresses",
+            description: "If checked, the integration will lookup IPv4 and IPv6 addresses",
+            default: true,
+            type: "boolean",
+            userCanEdit: true,
+            adminOnly: false
         }
-
     ]
 };
